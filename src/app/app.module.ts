01869c60358e5d368fc4e2fce3016363ff7fa7b8
athulya2000 <athulyav00@gmail.com>
2022-11-25 15:41:48 +0530

    view completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { RouterModule, RouterStateSnapshot, Routes, ROUTES, RoutesRecognized } from '@angular/router';
+import { ViewAllpostsComponent } from './view-allposts/view-allposts.component';
 
 const myRoute:Routes=[
 {
   path:"",
   component:AddPostComponent
+},
+{
+  path:"view",
+  component:ViewAllpostsComponent
 }
 ]
   
@@ -21,12 +26,13 @@ const myRoute:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    AddPostComponent
+    AddPostComponent,
+    ViewAllpostsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
